70af65f5daa795b5b31157aaa32c022d52e7311c
Patrick Schmidt <pschmidt@pasit.nl>
2020-10-29 12:27:08 +0100

    rest

--- MoreSerious/RestService/Controllers/PersonController.cs
+++ MoreSerious/RestService/Controllers/PersonController.cs
@@ -1,29 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Entities;
-using Fakes;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Repository.Interfaces;
 
 namespace RestService.Controllers
 {
-    //[Route("person")]
+    [Route("person")]
     [ApiController]
     public class PersonController : ControllerBase
     {
-        [Route("person/all")]
-        public List<Person> Get()
+        private IPersonRepository _repository;
+
+        public PersonController(IPersonRepository repo)
         {
-            PersonFake fk = new PersonFake();
-            return fk.Generate(40);
+            _repository = repo;
         }
-        [Route("person/{id}")]
-        public Person Get(int id)
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get([FromQuery]int skip = 0, [FromQuery]int take = 10)
         {
-            PersonFake fk = new PersonFake();
-            return fk.Generate(20).FirstOrDefault(p => p.ID == id);
+            var query = _repository.Get().Skip(skip).Take(take);
+            return Ok(query.ToList());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get(int id)
+        {
+            var query = _repository.Get().Where(p => p.ID == id);            
+            return Ok(query.FirstOrDefault());
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Post([FromBody]Person px)
+        {
+            // Voeg toe aan collectie
+            px.ID = 2000;
+            return Created($"/person/{px.ID}",px);
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Put(int id, [FromBody] Person px)
+        {
+            // Bestaat hij wel?
+            // Niet?
+            //return NotFound();
+            // Concurrency issue?
+            //return Conflict();
+            // Voeg toe aan collectie
+            px.ID = 2000;
+            return Accepted(px);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Bestaat hij wel?
+            // Niet?
+            //return NotFound();
+            // Voeg toe aan collectie
+            return Accepted();
         }
     }
 }
