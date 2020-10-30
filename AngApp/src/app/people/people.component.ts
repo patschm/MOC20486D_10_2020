65ca732a679640a90bff9a4427399e7007bce5e7
Patrick Schmidt <pschmidt@pasit.nl>
2020-10-30 12:02:13 +0100

    ang

---
+++ AngApp/src/app/people/people.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Person } from '../entities';
+
+@Component({
+  selector: 'app-people',
+  templateUrl: './people.html' ,
+  styles: [
+  ]
+})
+export class PeopleComponent implements OnInit 
+{
+  public people$: Observable<Person[]>;
+
+  constructor(private client:HttpClient) { }
+
+  ngOnInit(): void {
+    this.people$ = this.client.get<Person[]>("https://localhost:5001/person");
+  }
+
+}
